fd51933a11e0eb5ed34a4842db3504891abfb7a8
grammers <samuel_karlsson@home.se>
2018-09-13 15:12:22 +0200

    splited redirektion up and added stop conditions

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,64 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Joy.h"
 #include "geometry_msgs/Twist.h"
-
+#include <time.h>
 
 //constant setup variabels change thise values here
 #define NODE_NAME "engien_mgmt"
 #define JOY_SUB_NODE "joy"
 #define ADVERTISE_POWER "motor_power"
 #define BUFER_SIZE 200
-
+#define PUSH_SPEED 250 // minimal ms betvin toggel buton register
+#define TIME_OUT 500 // if no joy msg arives in thise time (ms) will it stop
 
 geometry_msgs::Twist vel_msg;
 ros::Publisher motor_power_pub;
 
+float x_ber = 0;
+float z_ber = 0;
 
+bool stop_time = true;
+bool stop_button = true;
+
+time_t s_interval = 0;
+time_t joy_timer;
+
+void pubEnginePower();
 
 // reseves new data from joy
 // TODO stor data and macke calebrations for mor fansy controling befor publiching
 void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
 {
-  vel_msg.linear.x = 100*(msg->axes[1]);
-  vel_msg.linear.z = 100*(msg->axes[4]);
+  x_ber = 100*(msg->axes[1]);
+  z_ber = 100*(msg->axes[4]);
 
-  motor_power_pub.publish(vel_msg);
+  if (msg->buttons[0] == 1 && (difftime(s_interval, time(NULL)) > PUSH_SPEED))
+  {
+	  stop_button = !stop_button;
+	  time(&s_interval);
+  }
+
+  time(&joy_timer);
+  s_interval++;
+  pubEnginePower();
+}
+
+// publiche when new stering diraktions is set
+void pubEnginePower()
+{
+	if ((difftime(joy_timer, time(NULL)) > TIME_OUT) && stop_button)
+	{
+		vel_msg.linear.x = x_ber;
+		vel_msg.linear.z = z_ber;
+		motor_power_pub.publish(vel_msg);
+	}
+	else
+	{
+		vel_msg.linear.x = 0;
+		vel_msg.linear.x = 0;
+		motor_power_pub.publish(vel_msg);
+	}
+	return;
 }
 
 int main(int argc, char **argv)
